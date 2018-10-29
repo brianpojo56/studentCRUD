a89bd86e0a3de79ff6488922b69fb0abf56ab69f
bellings <bellings@harris.com>
2018-10-29 09:37:50 -0400

    Service reaches out to remote server now

--- src/app/student.service.ts
+++ src/app/student.service.ts
@@ -1,17 +1,38 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { University } from './university';
 import { Student } from './student';
 import { UNIVERSITY } from './test-data';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json'})
+};
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class StudentService {
 
-  constructor() { }
+  private studentUrl = 'https://my-json-server.typicode.com/brianpojo56/students/StudentList';
+
+  constructor( private http: HttpClient) { }
 
   getStudents(): Observable<Student[]> {
-    return of(UNIVERSITY.StudentList);
+    return this.http.get<Student[]>(this.studentUrl)
+      .pipe(
+        tap(students => console.log('fetched students')),
+        catchError(this.handleError('getStudents', []))
+      );
+  }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    }
   }
 }
